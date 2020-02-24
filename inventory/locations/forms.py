47fafd01119cd55dc783e489044865dca5624a76
Ansari <shariq-anwar-ahmed.ansari@capgemini.com>
2020-02-24 09:37:45 +0530

    added features

--- inventory/locations/forms.py
+++ inventory/locations/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from inventory.models import Location
 
 class LocationForm(FlaskForm):
     location_name = StringField('Location Name', validators=[DataRequired()])
     submit = SubmitField('Add Location')
+
+    def validate_location_name(self, location_name):
+        location = Location.query.filter_by(location_name=location_name.data).first()
+        if location:
+            raise ValidationError('That location is available. Please choose a different one.')
--- inventory/products/forms.py
+++ inventory/products/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from inventory.models import Product
 
 class ProductForm(FlaskForm):
     product_name = StringField('Product Name', validators=[DataRequired()])
     submit = SubmitField('Add Product')
+
+    def validate_product_name(self, product_name):
+        product = Product.query.filter_by(product_name=product_name.data).first()
+        if product:
+            raise ValidationError('That product is available. Please choose a different one.')
